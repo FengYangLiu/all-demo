eb6541db8af82f37634d3af5c318dbbf88e205e5
lfy <coderlfy@163.com>
2019-05-05 17:42:00 +0800

    update：管理输出

--- webpack4_官网示例/webpack-demo/readme.md
+++ webpack4_官网示例/webpack-demo/readme.md
@@ -1,5 +1,7 @@
 # webpack4简单demo
 
+v4.30
+
 ## 起步
 
 主要是搭建基础架子，添加需要的依赖与基础讲解
@@ -7,3 +9,10 @@
 ## 资源管理
 
 对css/image/font/xml 的处理
+
+## 管理输出
+
+1. 通过动态的更改hash生成对应的bundle文件，修改配置等
+2. 如果新增入口或者添加入口会出现老文件没有更新入口，使用`HtmlWebpackPlugin`解决问题
+3. 由于每次生成文件，dist会有很多不相关的文件见,这时候就可以使用`clean-webpack-plugin`清理
+4. webpack 通过 manifest，可以追踪所有模块到输出 bundle 之间的映射
--- webpack4_官网示例/webpack-demo/src/index.js
+++ webpack4_官网示例/webpack-demo/src/index.js
@@ -1,22 +1,16 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './timg.jpeg';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
     let element = document.createElement('div');
   
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack','测试字体'], ' ');
-    element.classList.add('hello');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    let btn = document.createElement('button');
+    btn.innerHTML = '点击这里，然后查看 console！';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
-    // 添加图片
-    let image = new Image();
-    image.src = Icon;
-    element.appendChild(image);
-
-    console.log(Data);
-    
     return element;
   }
   
