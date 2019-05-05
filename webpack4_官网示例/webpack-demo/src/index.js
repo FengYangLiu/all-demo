5c62d012a4856832cc87a0c86258a14732f0e7ca
lfy <coderlfy@163.com>
2019-05-05 17:12:46 +0800

    update：资源管理

--- webpack4_官网示例/webpack-demo/src/index.js
+++ webpack4_官网示例/webpack-demo/src/index.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './timg.jpeg';
+import Data from './data.xml';
+
 function component() {
     let element = document.createElement('div');
   
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.innerHTML = _.join(['Hello', 'webpack','测试字体'], ' ');
+    element.classList.add('hello');
+
+    // 添加图片
+    let image = new Image();
+    image.src = Icon;
+    element.appendChild(image);
+
+    console.log(Data);
+    
     return element;
   }
   
